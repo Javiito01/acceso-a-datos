c45e8418fdfd68adc1a4f06a9d6c6877e50bc8ab
Javiito01 <jrgonzalezm01@outlook.es>
2025-09-11 01:13:33 +0200

    Actualización y Creación.
    README creado y actualizado HolaMundo.

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
+
 public class HolaMundo {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Addition: " + (a + b));
-        System.out.println("Subtraction: " + (a - b));
-        System.out.println("Multiplication: " + (a * b));
-        if (b != 0) {
-            System.out.println("Division: " + (a / b));
-        } else {
-            System.out.println("Division: Cannot divide by zero");
+        System.out.print("Introduce el primer número: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int b = scanner.nextInt();
+
+        System.out.println("Elige la operación (+, -, *, /): ");
+        String operacion = scanner.next();
+
+        switch (operacion) {
+            case "+":
+                System.out.println("Suma: " + (a + b));
+                break;
+            case "-":
+                System.out.println("Resta: " + (a - b));
+                break;
+            case "*":
+                System.out.println("Multiplicación: " + (a * b));
+                break;
+            case "/":
+                if (b != 0) {
+                    System.out.println("División: " + (a / b));
+                } else {
+                    System.out.println("División: No se puede dividir por cero");
+                }
+                break;
+            default:
+                System.out.println("Operación no válida");
         }
+
+        scanner.close();
     }
 }
